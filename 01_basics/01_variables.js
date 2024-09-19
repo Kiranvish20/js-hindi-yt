eaf81c5d21ad21740c20751920033081e08b37ff
Kiran Vishwakarma <kiran.vis.355@gmail.com>
2024-09-19 19:23:05 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,4 +2,6 @@ const accountId = 227456
 let accountEmail = "kiran@gmail.com"
 var accountPassword = "334455"
 
-console.log(accountId);
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword])
